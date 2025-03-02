a0809c2ce9d671c5e98dcbc4946030be00993246
Jacek <jmonka@edu.cdv.pl>
2025-03-02 18:01:21 +0100

    first commit

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>{{ message }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        message: String,
+    },
+    mounted() {
+        console.log('Props:', this.$props);
+    }
+};z
+</script>
