d7be789abc5ec325da25698c162a93d6c8461832
Materdeo <mateusz.borkowski22@gmail.com>
2025-03-07 20:39:23 +0100

    Includowanie stylowania i fontów

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,7 +8,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
     @routes
-    @vite(['resources/js/app.js'])
+    @vite('resources/js/app.js')
+    @vite('resources/css/app-old.scss')
     @inertiaHead
 
 </head>
