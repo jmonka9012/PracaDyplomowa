f7f579f106968a2a1161c59046520ae15a2ae92e
yen <notyourb42@gmail.com>
2025-03-09 15:26:24 +0100

    unit testy do renderowania strony i rejestracji

---
+++ tests/Unit/Auth/AuthRegisterTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Tests\Unit\Auth;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Support\Facades\Hash;
+use Tests\TestCase;
+
+class AuthRegisterTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    /** @test */
+    public function it_validates_and_creates_a_user_successfully()//sprawdza rejestracje
+    {
+        $data = [
+            'name' => 'John Doe',
+            'email' => 'john@example.com',
+            'password' => 'Password123',
+            'password_confirmation' => 'Password123'
+        ];
+
+        $response = $this->post(route('register'), $data);
+
+        $this->assertDatabaseHas('users', [
+            'name' => 'John Doe',
+            'email' => 'john@example.com',
+        ]);
+
+        $user = User::where('email', 'john@example.com')->first();
+        $this->assertTrue(Hash::check('Password123', $user->password));
+
+        $response->assertRedirect(route('home'));
+    }
+
+    /** @test */
+    public function it_fails_validation_with_invalid_data() // sprawdza działanie walidacji
+    {
+        $data = [
+            'name' => '',
+            'email' => 'invalid-email',
+            'password' => '123',
+            'password_confirmation' => '123' //
+        ];
+
+        $response = $this->post(route('register'), $data);
+
+        $response->assertSessionHasErrors(['name', 'email', 'password']);
+    }
+}
---
+++ tests/Unit/Auth/AuthRenderTest.php
@@ -0,0 +1,21 @@
+<?php
+namespace Tests\Unit\Auth;
+
+use Tests\TestCase;
+use Inertia\Testing\AssertableInertia;
+
+class AuthRenderTest extends TestCase
+{
+    /** @test */
+    public function it_renders_the_registration_form_with_correct_data()//sprawdze czy strona się renderuje, bez css, tylko czy wszystkie częsci są
+    {
+        $response = $this->get(route('register'));
+
+        // Check if the response is an Inertia response
+        $response->assertInertia(fn (AssertableInertia $inertia) => 
+            $inertia->component('Forms')
+                ->has('message')
+                ->has('csrf_token')
+        );
+    }
+}
