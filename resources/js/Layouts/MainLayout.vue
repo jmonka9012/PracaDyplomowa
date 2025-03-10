3da83c21d39d5467831b06eafe99167b80436610
Jacek <jmonka@edu.cdv.pl>
2025-03-10 20:31:04 +0100

    Dodanie przekazywania informacji do FE o stanie zalogowania

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,6 +1,13 @@
 <script setup>
 import MainFooter from "../Components/sections-new/MainFooter.vue";
 import MainHeader from "../Components/header/MainHeader.vue";
+
+const props = defineProps({
+    auth: Object,
+});
+
+console.log(props.auth.user);
+
 </script>
 
 <template>
