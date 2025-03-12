c9ac02046c5e472725ac3039e62f2a51dce11f99
Materdeo <mateusz.borkowski22@gmail.com>
2025-03-12 18:35:11 +0100

    Zmiany w formularzu rejestracji

--- resources/js/Pages/Forms.vue
+++ resources/js/Pages/Forms.vue
@@ -41,12 +41,25 @@ import blogBg from "~images/blog-bg.jpg";
             <form class="form" method="post" action="/register">
                 <input type="hidden" name="_token" :value="csrf_token" />
                 <div class="input-wrap d-flex flex-column col-12">
-                    <label for="register-username">Dane użytkownika *</label>
+                    <label for="register-username">Nazwa użytkownika *</label>
                     <input
                         type="text"
                         id="register-username"
                         name="name"
                         autocomplete="register-username"
+                        spellcheck="false"
+                        value=""
+                        required=""
+                        aria-required="true"
+                    />
+                </div>
+                <div class="input-wrap d-flex flex-column col-12">
+                    <label for="register-email">Email*</label>
+                    <input
+                        type="text"
+                        id="register-email"
+                        name="name"
+                        autocomplete="register-email"
                         spellcheck="false"
                         value=""
                         required=""
