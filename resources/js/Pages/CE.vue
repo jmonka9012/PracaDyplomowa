3ef011f39fea38d8d39a9879b15e240de19dec5a
Materdeo <mateusz.borkowski22@gmail.com>
2025-03-17 20:03:48 +0100

    Dodanie podstrony detalu

--- resources/js/Pages/CE.vue
+++ resources/js/Pages/CE.vue
@@ -1,17 +1,11 @@
+<script setup>
+
+</script>
+
 <template>
-    <div class="about">
-        <h1>CE dla Matiego</h1>
-    </div>
+
 </template>
 
-<style>
-@media (min-width: 1024px) {
-    .about {
-        min-height: 100vh;
-        display: flex;
-        align-items: center;
-    }
-}
+<style lang="scss">
+
 </style>
-<script setup>
-</script>
