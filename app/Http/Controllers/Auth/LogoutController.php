ecc3d39ffa4bb59ce5d0348671e16fb3bc1681da
yen <notyourb42@gmail.com>
2025-03-19 22:22:41 +0100

    mały update do route my account żeby nie zalogowani użytkownici nie mieli dostępu, logout pamięta na której strone jest użytkownik

--- app/Http/Controllers/Auth/LogoutController.php
+++ app/Http/Controllers/Auth/LogoutController.php
@@ -11,10 +11,33 @@ class LogoutController extends Controller
 {
     public function destroy(Request $request): RedirectResponse
     {
+        $previousUrl = $request->headers->get('referer');
+
         Auth::logout();
         $request->session()->invalidate();
         $request->session()->regenerateToken();
 
+        if ($this->isUrlAccessibleToGuests($previousUrl)) {
+            return redirect()->to($previousUrl);
+        }
+
         return redirect()->route('home');
+    }
+
+    protected function isUrlAccessibleToGuests(string $url): bool
+    {
+        $guestAccessibleRoutes = [
+            'home',
+            'about',
+            'contact',
+        ];
+
+        foreach ($guestAccessibleRoutes as $route) {
+            if (str_contains($url, route($route))) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
