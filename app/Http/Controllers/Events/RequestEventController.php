b110164fc42f43a8711467e6675cabe30c86b641
yen <notyourb42@gmail.com>
2025-03-23 14:13:41 +0100

    zmiana nazw zmiennych w BE w RequestEventController.php hotfix

--- app/Http/Controllers/Events/RequestEventController.php
+++ app/Http/Controllers/Events/RequestEventController.php
@@ -29,36 +29,36 @@ class RequestEventController extends Controller
         $request->merge(['image-path' => $imagePath]);
 
         $validatedData = $request->validate([
-            'event-name' => 'required|string|max:255|unique:events,event_name',
-            'event-slug' => 'string|max:255|unique:events,event_url',
-            'event-date' => 'date|required',
-            'event-start' => 'time|required',
-            'event-end' => 'time|required',
-            'contact-email' => 'string|max:255',
-            'contact-email_additional' => 'string|max:255',
-            'event-description' => 'max:65535',
-            'event-description-additional' => 'max:65535',
-            'event-location' => 'string|max:255',
-            'image-path' => 'string|max:255|nullable',
+            'event_name' => 'required|string|max:255|unique:events,event_name',
+            'event_url' => 'string|max:255|unique:events,event_url',
+            'event_date' => 'date|required',
+            'event_start' => 'time|required',
+            'event_end' => 'time|required',
+            'contact_email' => 'string|max:255',
+            'contact_email_additional' => 'string|max:255',
+            'event_description' => 'max:65535',
+            'event_description-additional' => 'max:65535',
+            'event_location' => 'string|max:255',
+            'image_path' => 'string|max:255|nullable',
         ], [
-            'events-name.unique' => 'Istnieję już wydarzenia z tą nazwą',
-            'events-name.max' => 'Nazwa wydarzenia jest zbyt długa',
-            'event-slug.unique' => 'Istnieje już wydarzenia z tym URLem',
-            'event-description.max' => 'Opis jest zbyt długi',
-            'event-description-additional.max'=> 'Dodtakowe informacje są zbyt długie',
+            'events_name.unique' => 'Istnieję już wydarzenia z tą nazwą',
+            'events_name.max' => 'Nazwa wydarzenia jest zbyt długa',
+            'event_slug.unique' => 'Istnieje już wydarzenia z tym URLem',
+            'event_description.max' => 'Opis jest zbyt długi',
+            'event_description_additional.max'=> 'Dodtakowe informacje są zbyt długie',
 
         ]);
         
         $event = Events::create([
-            'event_name'=> $validatedData['event-name'],
-            'event_url'=> $validatedData['event-url'],
-            'event_date'=> $validatedData['event-date'],
-            'event_start'=> $validatedData['event-start'],
-            'event_end'=> $validatedData['event-end'],
-            'contact_email'=> $validatedData['contact-email'],
-            'contact_email_additional'=> $validatedData['contact-email-additional'],
-            'event_location'=> $validatedData['event-location'],
-            'image_path'=> $validatedData['image-path'],
+            'event_name'=> $validatedData['event_name'],
+            'event_url'=> $validatedData['event_url'],
+            'event_date'=> $validatedData['event_date'],
+            'event_start'=> $validatedData['event_start'],
+            'event_end'=> $validatedData['event_end'],
+            'contact_email'=> $validatedData['contact_email'],
+            'contact_email_additional'=> $validatedData['contact_email_additional'],
+            'event_location'=> $validatedData['event_location'],
+            'image_path'=> $validatedData['image_path'],
         ]);
 
         return redirect()->route('home');
