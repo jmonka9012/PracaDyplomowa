c93f2be7b3dbb8c05cc7610c7a00b1d26bcd93dd
yen <notyourb42@gmail.com>
2025-04-01 15:32:11 +0200

    baza danych do biletów i powiązania bazy z resztą baz danych

---
+++ app/Models/Ticket.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+
+class Ticket extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'event_id',
+        'user_id',
+        'seat',
+        'hall_section',
+        'insured',
+    ];
+
+    protected $with = ['events','user'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function event()
+    {
+        return $this->belongsTo(Events::class);
+    }
+}
---
+++ database/migrations/2025_03_31_170801_tickets.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('event_id');
+            $table->unsignedBigInteger('user_id');
+            $table->string('seat');
+            $table->string('hall_section');
+            $table->boolean('insured')->default(false);
+            $table->timestamps();
+
+            $table->foreign('event_id')->references('id')->on('events');
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tickets');
+    }
+};
