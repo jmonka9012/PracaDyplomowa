b01561519158a1fe0cecda153ca6844572a18d5b
yen <notyourb42@gmail.com>
2025-04-01 18:38:41 +0200

    fix CE

--- app/Http/Controllers/CEController.php
+++ app/Http/Controllers/CEController.php
@@ -10,16 +10,12 @@ class CEController extends Controller
 {
     public function index(Request $request)
     {
-
-        $halls = Hall::with('sections')->get();
-
-        return Inertia::render('Jacek', [
-            'halls' => $halls,
-        ]);
-
         $routeName = $request->route()->getName();
         if ($routeName === 'jacek') {
-            return Inertia::render('Jacek');
+            $halls = Hall::with('sections')->get();
+            return Inertia::render('Jacek', [
+                'halls' => $halls,
+            ]);
         } else if ($routeName === 'ce') {
             return Inertia::render('CE');
         }
