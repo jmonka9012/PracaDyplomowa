af32d576d8f8ef72ad7957849f86df6603c17259
Materdeo <mateusz.borkowski22@gmail.com>
2025-04-01 19:58:26 +0200

    table dla eventów

--- resources/js/Pages/CE.vue
+++ resources/js/Pages/CE.vue
@@ -98,8 +98,8 @@ import hellsPit from "~images/hellspit.jpg";
             </form>
         </div>
     </section>
-    <HeroSmall :source="blogBg" title="Users"></HeroSmall>
 
+    <HeroSmall :source="blogBg" title="Wydarzenia"></HeroSmall>
     <section>
         <div class="container">
             <div class="col-12 d-flex align-items-center">
@@ -142,31 +142,31 @@ import hellsPit from "~images/hellspit.jpg";
                         </tr>
                     </thead>
                     <tbody>
-                        <tr class="user">
-                            <td class="user__select">
+                        <tr class="t-details">
+                            <td class="t-details__select">
                                 <input
                                     class="check"
                                     type="checkbox"
                                     name="Select &name user"
                                 />
                             </td>
-                            <td class="user__nick">
-                                <img :src="hellsPit" class="user__pic" />
+                            <td>
+                                <img :src="hellsPit" class="t-details__pic" />
                                 <a href="admin@gmail.com">Admin</a>
-                                <div class="user__options">
+                                <div class="t-details__options">
                                     <a href="#edit">Edytuj</a>
                                     <a href="#delete">Usuń</a>
                                     <a href="#view">Zobacz</a>
                                     <a href="#resetPass">Resetuj Hasło</a>
                                 </div>
                             </td>
-                            <td class="user__name">Admin Administratorski</td>
-                            <td class="user__mail">
+                            <td>Admin Administratorski</td>
+                            <td>
                                 <a href="admin@gmail.com">admin@gmail.com</a>
                             </td>
-                            <td class="user__role">Administrator</td>
-                            <td class="user__posts">21</td>
-                            <td class="user__id">05</td>
+                            <td>Administrator</td>
+                            <td>21</td>
+                            <td>05</td>
                         </tr>
                     </tbody>
                     <tfoot>
@@ -200,6 +200,133 @@ import hellsPit from "~images/hellspit.jpg";
                             <th>Rola</th>
                             <th>Posty?</th>
                             <th>ID</th>
+                        </tr>
+                    </tfoot>
+                </table>
+            </div>
+        </div>
+    </section>
+
+    <section>
+        <div class="container">
+            <div class="col-12 d-flex align-items-center">
+                <h1>Wydarzenia</h1>
+                <a href="#" class="ml-20px btn btn-md">Dodaj nowe</a>
+            </div>
+            <div class="col-12 mt-30px mb-30px overflow-x-scroll">
+                <table class="table-nowrap">
+                    <thead>
+                        <tr>
+                            <th class="t-details-events__input">
+                                <input
+                                    class="check"
+                                    type="checkbox"
+                                    name="Select all users"
+                                />
+                            </th>
+                            <th class="t-details-events__main-img">
+                                Obrazek Główny
+                            </th>
+                            <th class="t-details-events__name">
+                                <a href="#username-sort"
+                                    >Nazwa
+                                    <span class="sorters">
+                                        <span class="sort sort-asc fa"></span>
+                                        <span class="sort sort-desc fa"></span>
+                                    </span>
+                                </a>
+                            </th>
+                            <th class="t-details-events__url">Url</th>
+                            <th class="t-details-events__date">Data</th>
+                            <th class="t-details-events__start">
+                                Początek (h)
+                            </th>
+                            <th class="t-details-events__end">Koniec (h)</th>
+                            <th class="t-details-events__loc">Lokalizacja</th>
+                            <th class="t-details-events__e-main">Email gł.</th>
+                            <th class="t-details-events__e-add">
+                                Email dodatkowy
+                            </th>
+                            <th class="t-details-events__desc">Opis</th>
+                            <th class="t-details-events__mr-info">
+                                Więcej informacji
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr class="t-details">
+                            <td class="t-details__select">
+                                <input
+                                    class="check"
+                                    type="checkbox"
+                                    name="Select @name event"
+                                />
+                            </td>
+                            <td>
+                                <img
+                                    :src="hellsPit"
+                                    class="t-details__pic mr-0"
+                                />
+                            </td>
+                            <td>
+                                <a href="#event-link">Wydarzenie numer 1</a>
+                                <div class="t-details__options">
+                                    <a href="#edit">Edytuj</a>
+                                    <a href="#delete">Usuń</a>
+                                    <a href="#view">Zobacz</a>
+                                </div>
+                            </td>
+                            <td>/Lorem-ipsum-1</td>
+                            <td>1.04</td>
+                            <td>16:00</td>
+                            <td>24:00</td>
+                            <td>Sala nr 1</td>
+                            <td><a href="#email-main">Główny email</a></td>
+                            <td><a href="#email-add">Dodatkowy email</a></td>
+                            <td class="t-details-events__td-desc">
+                                Lorem ipsum dolor sit, amet consectetur
+                                adipisicing elit. Expedita suscipit nisi cum
+                                reiciendis exercitationem. Placeat magnam
+                                explicabo consequatur quam possimus dolore iste
+                                harum eum perferendis repellendus, dolorem
+                                cupiditate error beatae?
+                            </td>
+                            <td class="t-details-events__td-desc">
+                                Lorem ipsum dolor sit amet, consectetur
+                                adipisicing elit. Sed culpa vero dicta facere
+                                quaerat nam ullam fuga harum exercitationem
+                                illum?
+                            </td>
+                        </tr>
+                    </tbody>
+                    <tfoot>
+                        <tr>
+                            <th>
+                                <input
+                                    class="check"
+                                    type="checkbox"
+                                    name="Select all users"
+                                />
+                            </th>
+                            <th>
+                                <a href="#username-sort"
+                                    >Nazwa
+                                    <span class="sorters">
+                                        <span class="sort sort-asc fa"></span>
+                                        <span class="sort sort-desc fa"></span>
+                                    </span>
+                                </a>
+                            </th>
+                            <th>Url</th>
+                            <th>Obrazek Główny</th>
+                            <th>Data</th>
+                            <th>Początek (h)</th>
+                            <th>Koniec (h)</th>
+                            <th>Lokalizacja</th>
+                            <th>Email gł.</th>
+                            <th>Email dodatkowy</th>
+                            <th>Opis</th>
+                            <th>Więcej informacji</th>
                         </tr>
                     </tfoot>
                 </table>
