3bbfde419409802e08ed34ede4e0090c755dbbaf
pgalimski <pgalimski@edu.cdv.pl>
2025-04-04 17:30:55 +0200

    register_test_dwa

--- selenium-tests/register_test/register_test.js
+++ selenium-tests/register_test/register_test.js
@@ -11,25 +11,19 @@ import chrome from 'selenium-webdriver/chrome.js';
     options.addArguments('--remote-debugging-port=9222');
 
     // Uruchomienie przeglądarki
-    driver = await new Builder()
-      .forBrowser('chrome')
-      .setChromeOptions(options)
-      .build();
-
-    // Ustawienie rozmiaru okna
+    driver = await new Builder().forBrowser('chrome').setChromeOptions(options).build();
     await driver.manage().window().setRect({ width: 1400, height: 1000 });
     console.log('Przeglądarka uruchomiona.');
 
     // Przejście do strony rejestracji
     await driver.get('http://lvi.ddev.site/rejestracja');
-    console.log('Strona rejestracji załadowana.');
+    console.log('Załadowano stronę rejestracji.');
 
-    // Krótkie oczekiwanie i przewinięcie strony
-    await driver.sleep(2000);
+    await driver.sleep(2000); // Krótkie oczekiwanie
     await driver.executeScript('window.scrollBy(0, window.innerHeight / 2);');
-    console.log('Przewinięto stronę w dół.');
+    console.log('Przewinięto stronę.');
 
-    // Generowanie przykładowych danych użytkownika
+    // Generowanie danych konta
     const firstNames = ['Anna', 'Marek', 'Kamil', 'Ola', 'Michał'];
     const lastNames = ['Kowalska', 'Nowak', 'Wiśniewska', 'Kamiński', 'Zieliński'];
     const first = firstNames[Math.floor(Math.random() * firstNames.length)];
@@ -38,64 +32,64 @@ import chrome from 'selenium-webdriver/chrome.js';
     const email = `${login.toLowerCase()}@fikcja.pl`;
     const password = 'Test1234!';
 
-    console.log(`Wygenerowane dane: login: ${login}, e-mail: ${email}, hasło: ${password}`);
+    console.log(`Dane konta: login: ${login}, email: ${email}, hasło: ${password}`);
 
     // Wypełnianie formularza rejestracji
-    const inputFields = await driver.findElements(By.css('form:nth-of-type(2) input'));
-    if (inputFields.length >= 4) {
-      console.log('Wypełnianie formularza rejestracji...');
-      await inputFields[0].sendKeys(login);      // login
-      await inputFields[1].sendKeys(email);      // e-mail
-      await inputFields[2].sendKeys(password);   // hasło
-      await inputFields[3].sendKeys(password);   // powtórzenie hasła
-      console.log('Formularz rejestracji wypełniony.');
+    const registrationFields = await driver.findElements(By.css('form:nth-of-type(2) input'));
+    if (registrationFields.length >= 6) {
+      console.log('Wypełniam formularz rejestracji...');
+      await registrationFields[0].sendKeys(login);       // Login
+      await registrationFields[1].sendKeys(email);       // Email
+      await registrationFields[2].sendKeys(first);       // Imię
+      await registrationFields[3].sendKeys(last);        // Nazwisko
+      await registrationFields[4].sendKeys(password);    // Hasło
+      await registrationFields[5].sendKeys(password);    // Powtórz hasło
     } else {
-      console.error('Nie znaleziono wszystkich pól formularza.');
+      console.error('Nie znaleziono wszystkich pól formularza rejestracji.');
       return;
     }
 
-    // Zaznaczenie checkboxa z regulaminem
-    const checkbox = await driver.findElement(By.css('form:nth-of-type(2) input[type="checkbox"]'));
-    await checkbox.click();
-    console.log('Zaznaczono checkbox regulaminu.');
+    // Zgoda na regulamin
+    const termsCheckbox = await driver.findElement(By.css('form:nth-of-type(2) input[type="checkbox"]'));
+    await termsCheckbox.click();
+    console.log('Zaznaczono zgodę na regulamin.');
     await driver.sleep(1000);
 
-    // Kliknięcie przycisku "ZAREJESTRUJ SIĘ"
-    const registerBtn = await driver.findElement(By.css('form:nth-of-type(2) input[type="submit"][value="ZAREJESTRUJ SIĘ"]'));
-    await registerBtn.click();
-    console.log('Kliknięto "ZAREJESTRUJ SIĘ".');
+    // Kliknięcie przycisku rejestracji
+    const registerButton = await driver.wait(until.elementLocated(By.css('form:nth-of-type(2) input[type="submit"]')), 5000);
+    await registerButton.click();
+    console.log('Kliknięto przycisk rejestracji.');
 
-    // Czekanie na komunikat potwierdzający
+    // Sprawdzenie komunikatu po rejestracji
     await driver.sleep(3000);
-    const bodyText = await driver.findElement(By.tagName('body')).getText();
-    const successMessage = bodyText.split('\n').find(line => /utworzono|sukces|zarejestrowano|konto/i.test(line));
-    if (successMessage) {
-      console.log('Rejestracja zakończona sukcesem:', successMessage);
+    const pageContent = await driver.findElement(By.tagName('body')).getText();
+    const registrationSuccess = pageContent.split('\n').find(line => /utworzono|sukces|zarejestrowano|konto|Moje konto/i.test(line));
+    if (registrationSuccess) {
+      console.log('Rejestracja zakończona sukcesem:', registrationSuccess);
     } else {
       console.error('Nie znaleziono komunikatu potwierdzającego rejestrację.');
     }
 
-    // Przejście do zakładki logowania
-    const loginTab = await driver.findElement(By.linkText('Login'));
-    await loginTab.click();
+    // Bez klikania linku – przechodzi bezpośrednio do strony logowania, druga weryfikacja
+    await driver.get('http://lvi.ddev.site/login');
     console.log('Przejście do strony logowania.');
-    await driver.sleep(2000);
+
+    await driver.sleep(2000); // Krótkie oczekiwanie
 
     // Wypełnianie formularza logowania
     const loginFields = await driver.findElements(By.css('form:nth-of-type(1) input'));
     if (loginFields.length >= 2) {
-      console.log('Wypełnianie formularza logowania...');
-      await loginFields[0].sendKeys(login);    // login
-      await loginFields[1].sendKeys(password); // hasło
-      console.log('Formularz logowania wypełniony.');
+      console.log('Wypełniam formularz logowania...');
+      await loginFields[0].sendKeys(login);    // Login
+      await loginFields[1].sendKeys(password); // Hasło
     } else {
       console.error('Nie znaleziono pól formularza logowania.');
       return;
     }
 
-    // Kliknięcie przycisku "ZALOGUJ SIĘ"
-    const loginBtn = await driver.findElement(By.css('form:nth-of-type(1) input[type="submit"][value="ZALOGUJ SIĘ"]'));
-    await loginBtn.click();
+    // Kliknięcie "ZALOGUJ SIĘ"
+    const loginButton = await driver.findElement(By.css('form:nth-of-type(1) input[type="submit"]'));
+    await loginButton.click();
     console.log('Kliknięto "ZALOGUJ SIĘ".');
     await driver.sleep(3000);
 
@@ -108,14 +102,11 @@ import chrome from 'selenium-webdriver/chrome.js';
       console.error('Nie znaleziono komunikatu "Witaj ponownie".');
     }
 
-    // Zakończenie testu i zamknięcie przeglądarki
+    // Zakończenie
     await driver.quit();
     console.log('Test zakończony pomyślnie.');
 
   } catch (err) {
-    // Obsługa błędów:
-    // Jeśli wystąpi błąd, zostanie on wypisany w konsoli.
-    // Jeżeli przeglądarka została już uruchomiona, zostanie zamknięta.
     console.error('Błąd w trakcie testu:', err);
     if (driver) await driver.quit();
   }
