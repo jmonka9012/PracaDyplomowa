f202e128a7b2a9dc60cfb13026d380910ea8cafb
Materdeo <mateusz.borkowski22@gmail.com>
2025-04-10 20:51:35 +0200

    wyrównanie eventów na blogu

--- resources/js/Components/sections-new/EventsAlt.vue
+++ resources/js/Components/sections-new/EventsAlt.vue
@@ -1,30 +1,43 @@
 <script setup>
 defineProps({
-	latest: Array,
+    latest: Array,
 });
 </script>
 
 <template>
-	<div :class="['ev-cont ev-cont-lg-three', evContClass]">
-		<div v-for="event in latest" :key="event.id" class="event-item">
-			<div
-				class="d-flex flex-row flex-wrap-wrap align-items-center pt-25px mb-30px bt-1 b-secondary"
-			>
-				<h6 class="mb-30px">
-					<a class="hover-underline-thick" :href="event.link.href">
-						{{ event.title }}
-					</a>
-				</h6>
-				<p class="event-date">
-					{{ event.date }}
-				</p>
-				<span class="divider divider-dark"></span>
-				<p class="event-location ff-krona">{{ event.location }}</p>
-			</div>
-			<div class="relative">
-				<img :src="event.src" alt="" class="event-img" />
-				<a :href="event.link.href" class="link-stretched"></a>
-			</div>
-		</div>
-	</div>
+    <div :class="['ev-cont ev-cont-lg-three', evContClass]">
+        <div
+            v-for="event in latest"
+            :key="event.id"
+            class="event-item bt-1 b-secondary pt-25px"
+        >
+            <h6>
+                <a class="hover-underline-thick" :href="event.link.href">
+                    {{ event.title }}
+                </a>
+            </h6>
+            <div class="d-flex flex-row h-fit">
+                <p class="event-date">
+                    {{ event.date }}
+                </p>
+                <span class="divider divider-dark"></span>
+                <p class="event-location ff-krona">{{ event.location }}</p>
+            </div>
+            <div class="relative mt-auto">
+                <img :src="event.src" alt="" class="event-img event-img-blog" />
+                <a :href="event.link.href" class="link-stretched"></a>
+            </div>
+        </div>
+    </div>
 </template>
+
+<style lang="scss">
+.event-item {
+    display: grid;
+    grid-template-rows: 1fr 30px 270px;
+    grid-row-gap: 30px;
+}
+.event-img-blog {
+    margin-bottom: 0;
+}
+</style>
