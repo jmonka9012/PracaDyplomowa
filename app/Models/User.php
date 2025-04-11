3ffe5e246de99f44ce40554a288a97c1aabea4e1
yen <notyourb42@gmail.com>
2025-04-11 06:26:54 +0200

    zmiany w rolach userów

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,12 +2,14 @@
 
 namespace App\Models;
 
+use App\Enums\UserRole;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use App\Models\Blog\BlogAuthor;
 use App\Models\Tickets\Ticket;
+
 
 class User extends Authenticatable implements MustVerifyEmail
 {
@@ -26,6 +28,10 @@ class User extends Authenticatable implements MustVerifyEmail
         'first_name',
         'last_name',
         'email_verified_at',
+    ];
+
+    protected $casts = [
+        'role' => UserRole::class,
     ];
 
     /**
@@ -59,5 +65,10 @@ class User extends Authenticatable implements MustVerifyEmail
     public function author()
     {
         return $this->hasOne(BlogAuthor::class);
+    }
+
+    public function getPermissionLevel()
+    {
+        return UserRole::from($this->attributes['role'])->permissionLevel();
     }
 }
