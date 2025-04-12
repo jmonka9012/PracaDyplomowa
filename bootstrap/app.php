470af9079ac10d8cecd16542116c34924ec74953
yen <notyourb42@gmail.com>
2025-04-12 15:41:50 +0200

    middleware do autoryzacji użytkownika, na razie tylko na stronach administratorskich

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,7 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use App\Http\Middleware\HandleInertiaRequests;
-
+use App\Http\Middleware\CheckRolePermission;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -15,6 +15,30 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->web(append: [
             HandleInertiaRequests::class,
+        ]);
+
+        $middleware->alias([
+            'role' => CheckRolePermission::class,
+        ]);
+
+        $middleware->group('company_team', [
+            'auth',
+            'role:admin,moderator,redactor'
+        ]);
+
+        $middleware->group('organizers', [
+            'auth',
+            'role:admin,moderator,redactor,organizer'
+        ]);
+
+        $middleware->group('verified_users', [
+            'auth',
+            'role:admin,moderator,redactor,organizer,verified_user'
+        ]);
+
+        $middleware->group('unverified_users', [
+            'auth',
+            'role:admin,moderator,redactor,organizer,verified_user,unverified_user'
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
--- routes/admin.php
+++ routes/admin.php
@@ -8,13 +8,28 @@ use App\Http\Controllers\Admin\CustomerServiceController;
 use App\Http\Controllers\Admin\PendingEventsController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/admin/uzytkownicy', [ManageUsersController::class, 'index'])->name('admin.users');
-Route::get('/admin/uzytkownicy/data', [ManageUsersController::class, 'showData'])->name('admin.users.data');
+Route::prefix('admin')->middleware('company_team')->group(function() {
+      Route::get('/uzytkownicy', [ManageUsersController::class, 'index'])
+            ->name('admin.users');
 
-Route::get('/admin/wydarzenia', [PendingEventsController::class, 'index'])->name('admin.events');
-Route::get('/admin/wydarzenia/data', [PendingEventsController::class, 'showData'])->name('admin.events.data');
+      Route::get('/uzytkownicy/data', [ManageUsersController::class, 'showData'])
+            ->name('admin.users.data');
 
-Route::get('/admin/dodaj-post', [RequestBlogController::class, 'index'])->name('admin.add-post');
-Route::get('/admin/zarzadzaj-postami', [ManagePostsController::class, 'index'])->name('admin.posts');
-Route::get('/admin', [AdminHomeController::class, 'index'])->name('admin');
-Route::get('/admin/obsluga-klienta', [CustomerServiceController::class, 'index'])->name('admin.customer-service');
+      Route::get('/wydarzenia', [PendingEventsController::class, 'index'])
+            ->name('admin.events');
+
+      Route::get('/wydarzenia/data', [PendingEventsController::class, 'showData'])
+            ->name('admin.events.data');
+
+      Route::get('/dodaj-post', [RequestBlogController::class, 'index'])
+            ->name('admin.add-post');
+
+      Route::get('/zarzadzaj-postami', [ManagePostsController::class, 'index'])
+            ->name('admin.posts');
+
+      Route::get('/', [AdminHomeController::class, 'index'])
+            ->name('admin');
+
+      Route::get('/obsluga-klienta', [CustomerServiceController::class, 'index'])
+            ->name('admin.customer-service');
+});
