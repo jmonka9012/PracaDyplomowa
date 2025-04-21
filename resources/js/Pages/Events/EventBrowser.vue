e8f5ef7ac21db1a3a07e9b14c9aa0b48df485afc
Materdeo <mateusz.borkowski22@gmail.com>
2025-04-21 18:38:23 +0200

    paginacja eventy

--- resources/js/Pages/Events/EventBrowser.vue
+++ resources/js/Pages/Events/EventBrowser.vue
@@ -13,16 +13,38 @@ console.log(props);
 </script>
 
 <template>
-
     <section class="pb-75px">
         <div class="container flex-column align-items-center">
             <p class="sub-title sub-title-lprpl mb-20px">bilety na</p>
             <h3 class="title-1 mb-20px">Przyszłe wydarzenia</h3>
             <Events :events="props.events.data" />
+
+            <div class="event-pagination">
+                <ul>
+                    <li class="direction">
+                        <!-- ukryć jak jest page 1 -->
+                        <a href="#forward">
+                            <div class="d-flex">
+                                <i class="fa fa-chevron-left"></i>
+                                <i class="fa fa-chevron-left"></i>
+                            </div>
+                            Powróć
+                        </a>
+                    </li>
+                    <li class="page page-current"><a href="#1">1</a></li>
+                    <li class="page"><a href="#2">2</a></li>
+                    <li class="direction">
+                        <a href="#forward"
+                            >Dalej
+                            <div class="d-flex">
+                                <i class="fa fa-chevron-right"></i>
+                                <i class="fa fa-chevron-right"></i></div
+                        ></a>
+                    </li>
+                </ul>
+            </div>
         </div>
     </section>
 </template>
 
-<style scoped lang="scss">
-
-</style>
+<style scoped lang="scss"></style>
