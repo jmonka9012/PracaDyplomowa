5d6ed5273e60d6bbfc6268ddd4ecf479e938d261
Materdeo <mateusz.borkowski22@gmail.com>
2025-04-21 21:25:31 +0200

    wstępne stylowanie about us hero

--- resources/js/Components/sections-new/AboutUsHero.vue
+++ resources/js/Components/sections-new/AboutUsHero.vue
@@ -1,10 +1,20 @@
 <script setup>
-document.addEventListener('DOMContentLoaded', () => {
-    const animatedStickySections = document.querySelectorAll('.animated-sticky-section');
+import ctaCloud from "~images/cta-cloud.png";
+import spiral from "~images/spiral.png";
+import IconRotate from "@/Components/icons/IconRotate.vue";
+
+document.addEventListener("DOMContentLoaded", () => {
+    const animatedStickySections = document.querySelectorAll(
+        ".animated-sticky-section"
+    );
 
     animatedStickySections.forEach((section) => {
-        const stickyChild = section.querySelector('.animated-sticky-section__section');
-        const animationTarget = section.querySelector('.animated-sticky-section__target');
+        const stickyChild = section.querySelector(
+            ".animated-sticky-section__section"
+        );
+        const animationTarget = section.querySelector(
+            ".animated-sticky-section__target"
+        );
 
         const colorBottom = animationTarget.dataset.from;
         //const colorBottom = '#DCDCDC';
@@ -15,24 +25,27 @@ document.addEventListener('DOMContentLoaded', () => {
         const lettersArray = [];
 
         function GetScrollPercentage() {
-            return (stickyChild.offsetTop) / (section.offsetHeight - stickyChild.offsetHeight);
+            return (
+                stickyChild.offsetTop /
+                (section.offsetHeight - stickyChild.offsetHeight)
+            );
         }
 
         function CreateSpans(target) {
             if (!target) return;
 
             const text = target.textContent.trim();
-            target.textContent = '';
+            target.textContent = "";
 
-            text.split(' ').forEach((wordText, wordIndex) => {
-                const wordSpan = document.createElement('span');
-                wordSpan.classList.add('word');
+            text.split(" ").forEach((wordText, wordIndex) => {
+                const wordSpan = document.createElement("span");
+                wordSpan.classList.add("word");
 
                 wordsArray.push(wordSpan);
 
                 [...wordText].forEach((letter) => {
-                    const letterSpan = document.createElement('span');
-                    letterSpan.classList.add('letter');
+                    const letterSpan = document.createElement("span");
+                    letterSpan.classList.add("letter");
                     letterSpan.textContent = letter;
 
                     lettersArray.push(letterSpan);
@@ -42,15 +55,16 @@ document.addEventListener('DOMContentLoaded', () => {
 
                 target.appendChild(wordSpan);
 
-                if (wordIndex < text.split(' ').length - 1) {
-                    target.appendChild(document.createTextNode(' '));
+                if (wordIndex < text.split(" ").length - 1) {
+                    target.appendChild(document.createTextNode(" "));
                 }
             });
         }
 
         function UpdateText() {
             if (GetScrollPercentage() <= 1) {
-                const sentenceIndex = lettersArray.length * GetScrollPercentage();
+                const sentenceIndex =
+                    lettersArray.length * GetScrollPercentage();
                 const letterIndex = Math.floor(sentenceIndex);
                 // const letterVisibility = sentenceIndex - letterIndex;
 
@@ -69,33 +83,52 @@ document.addEventListener('DOMContentLoaded', () => {
         }
 
         CreateSpans(animationTarget);
-        window.addEventListener('scroll', UpdateText);
+        window.addEventListener("scroll", UpdateText);
         UpdateText();
     });
 });
-
 </script>
 
 <template>
-
-    <div class="animated-sticky-section">
+    <div class="animated-sticky-section bg-primary">
         <section class="animated-sticky-section__section as-hero">
-            <div class="container ">
-                <h1 data-from="red" data-to="yellow" class="animated-sticky-section__target">Poznaj nasz nowoczesny obiekt</h1>
+            <div class="as-iconrotate">
+                <IconRotate
+                    :background="`url(${spiral})`"
+                    rotateText="- Custom Rotating Text - Custom Rotating Text 2 "
+                />
+            </div>
+
+            <div class="container">
+                <h1
+                    data-from="red"
+                    data-to="yellow"
+                    class="animated-sticky-section__target as-heading"
+                >
+                    Poznaj nasz nowoczesny obiekt
+                </h1>
+            </div>
+            <div class="as-cloud">
+                <img :src="ctaCloud" alt="" />
             </div>
         </section>
     </div>
-
 </template>
 
 <style scoped lang="scss">
+@use "~css/mixin.scss";
+
 .animated-sticky-section {
-    height: 300vh;
+    height: 200vh;
+    @include mixin.media-breakpoint-up(lg) {
+        height: 300vh;
+    }
     position: relative;
+
     &__target {
         .letter {
             transition: color 0.8s;
-        }// nie działa?
+        } // nie działa?
     }
     &__section {
         position: sticky;
@@ -104,14 +137,56 @@ document.addEventListener('DOMContentLoaded', () => {
         display: flex;
         align-items: center;
         justify-content: center;
+        overflow: hidden;
     }
 }
 
 .letter {
     transition: color 0.8s ease-in-out;
-}// nie działa?
+} // nie działa?
 
 .as-hero {
+    background-image: url("");
     background-color: var(--primary);
+    background-image: url("~images/cta-bg.jpg");
+    background-repeat: no-repeat;
+    background-size: contain;
+    background-position: center;
+}
+.as-heading {
+    line-height: 1.1;
+    text-align: center;
+    font-size: 40px;
+    @include mixin.media-breakpoint-up(md) {
+        font-size: 80px;
+    }
+    @include mixin.media-breakpoint-up(lg) {
+        font-size: 100px;
+    }
+}
+.as-iconrotate {
+    position: absolute;
+    top: 100px;
+    left: 50px;
+    @include mixin.media-breakpoint-down(xl) {
+        width: 150px;
+        top: 50px;
+    }
+    .icon-rotate-svg {
+    }
+}
+.as-cloud {
+    position: absolute;
+    top: 65%;
+    left: -300px;
+    width: 100%;
+    z-index: 1;
+    min-width: 800px;
+    @include mixin.media-breakpoint-up(md) {
+        left: -25%;
+        min-width: 100%;
+    }
+    @include mixin.media-breakpoint-up(lg) {
+    }
 }
 </style>
