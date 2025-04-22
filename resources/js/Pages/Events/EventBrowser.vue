2a6347540b69c96ebd181cd830517900b48c7256
Jacek Mońka <jmonka@edu.cdv.pl>
2025-04-22 21:36:13 +0200

    Integracja paginacji w EventBrowser.vue

--- resources/js/Pages/Events/EventBrowser.vue
+++ resources/js/Pages/Events/EventBrowser.vue
@@ -1,5 +1,6 @@
 <script setup>
 import Events from "@/Components/Sections/Events.vue";
+import { Link } from "@inertiajs/vue3";
 
 import eventsBg from "~images/events-bg-1.jpg";
 
@@ -8,8 +9,19 @@ const props = defineProps({
         type: Array,
         required: true,
     },
+    genres: {
+        type: Array,
+        required: true,
+    }
 });
+
+function isCurrentPage(pageId) {
+    console.log(pageId);
+    return window.location.href.endsWith(pageId);
+}
+
 console.log(props);
+console.log(route('event.browser'));
 </script>
 
 <template>
@@ -17,12 +29,12 @@ console.log(props);
         <div class="container flex-column align-items-center">
             <p class="sub-title sub-title-lprpl mb-20px">bilety na</p>
             <h3 class="title-1 mb-20px">Przyszłe wydarzenia</h3>
-            <Events :events="props.events" />
+            <Events :events="props.events.data" />
 
             <div class="event-pagination">
-                <ul>
-                    <li class="direction">
-                        <!-- ukryć jak jest page 1 -->
+                <ul class="ml-auto mr-auto">
+<!--                    <li class="direction">
+                        &lt;!&ndash; ukryć jak jest page 1 &ndash;&gt;
                         <a href="#forward">
                             <div class="d-flex">
                                 <i class="fa fa-chevron-left"></i>
@@ -30,17 +42,18 @@ console.log(props);
                             </div>
                             Powróć
                         </a>
+                    </li>-->
+                    <li :key="page" class="page" :class="{'page-current': isCurrentPage(page.label)}" v-for="page in events.meta.links">
+                        <Link :href="page.url" v-html="page.label"></Link>
                     </li>
-                    <li class="page page-current"><a href="#1">1</a></li>
-                    <li class="page"><a href="#2">2</a></li>
-                    <li class="direction">
+<!--                    <li class="direction">
                         <a href="#forward"
                             >Dalej
                             <div class="d-flex">
                                 <i class="fa fa-chevron-right"></i>
                                 <i class="fa fa-chevron-right"></i></div
                         ></a>
-                    </li>
+                    </li>-->
                 </ul>
             </div>
         </div>
