633747a32e1637175e591beca799e6f2a8e3534e
Jacek Mońka <jmonka@edu.cdv.pl>
2025-04-22 21:48:47 +0200

    podłączenie socialek

--- resources/js/Pages/Events/EventBrowser.vue
+++ resources/js/Pages/Events/EventBrowser.vue
@@ -25,7 +25,7 @@ console.log(route('event.browser'));
 </script>
 
 <template>
-    <section class="pb-75px">
+    <section class="pb-75px pt-75px">
         <div class="container flex-column align-items-center">
             <p class="sub-title sub-title-lprpl mb-20px">bilety na</p>
             <h3 class="title-1 mb-20px">Przyszłe wydarzenia</h3>
