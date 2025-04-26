f0c2a6413a31dd72685601c00f4fd4ec9065b985
Jacek Mońka <jmonka@edu.cdv.pl>
2025-04-26 21:03:00 +0200

    Wstępne naniesienie danych na szablon bloga

--- resources/js/Pages/Blog/BlogSingle.vue
+++ resources/js/Pages/Blog/BlogSingle.vue
@@ -1,14 +1,26 @@
 <script setup>
 import PostQuery from "../../Components/Sections/PostQuery.vue";
 import HeroSmall from "@/Components/Sections/Hero-small.vue";
+import { Link } from "@inertiajs/vue3";
 
 import blogBg from "~images/blog-bg.jpg";
+
+const props = defineProps({
+    blog_post: {
+        type: Array,
+        required: true
+    }
+});
+
+const post = props.blog_post.data;
+
+console.log(props.blog_post);
 
 </script>
 
 <template>
 
-    <HeroSmall :source="blogBg" title="Tytuł wpisu"></HeroSmall>
+    <HeroSmall :source="`/storage/${post.thumbnail_path}`" :title="post.blog_post_name"></HeroSmall>
     <section>
         <div class="container">
             <div class="col-12 d-flex flex-lg-row row-gap-30px">
@@ -16,23 +28,14 @@ import blogBg from "~images/blog-bg.jpg";
                     <div
                         class="d-flex flex-row flex-wrap-wrap justify-content-center align-items-center bg-primary pt-12px pb-12px pl-6px pr-6px column-gap-5px border-radius-12px text-white"
                     >
-                        <!-- Tutaj potrzebny będzie loop żeby tworzyć kategorie -->
-                        <a class="hover-text" href="/kategoria/Bez-kategorii"
-                        >Bez kategorii,
-                        </a>
-                        <a class="hover-text" href="/kategoria/Bez-kategorii"
-                        >Lorem Ipsum,
-                        </a>
-                        <a class="hover-text" href="/kategoria/Bez-kategorii"
-                        >Lorem Ipsum Dolor</a
-                        >
+                        <Link>{{ post.blog_post_type }}</Link>
                     </div>
                 </div>
                 <div
                     class="col-12 col-lg-4 d-flex justify-content-center align-items-center"
                 >
                     <i class="fa fa-calendar mr-5px"></i>
-                    <span>2025-01-01</span>
+                    <span>{{post.updated_at}}</span>
                 </div>
                 <div
                     class="col-12 col-lg-4 d-flex justify-content-center align-items-center"
@@ -44,69 +47,26 @@ import blogBg from "~images/blog-bg.jpg";
 
             <div
                 class="col-12 d-flex flex-column align-items-center justify-content-center mt-lg-80px mt-50px"
+                v-html="post.blog_post_content"
             >
-                <h1 class="mb-30px">Lorem ipsum</h1>
-                <p class="mb-30px">
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                    Sequi cumque incidunt earum architecto dolor sit accusamus,
-                    magnam optio eius repellat labore molestias recusandae vitae
-                    delectus itaque. Facilis, et quis labore sint eligendi fuga
-                    unde quam quaerat dolorem voluptatum, alias rerum itaque.
-                    Nihil accusantium placeat consectetur numquam pariatur?
-                    Maxime, laborum doloribus? Lorem ipsum dolor sit, amet
-                    consectetur adipisicing elit. Sequi cumque incidunt earum
-                    architecto dolor sit accusamus, magnam optio eius repellat
-                    labore molestias recusandae vitae delectus itaque. Facilis,
-                    et quis labore sint eligendi fuga unde quam quaerat dolorem
-                    voluptatum, alias rerum itaque. Nihil accusantium placeat
-                    consectetur numquam pariatur? Maxime, laborum doloribus?
-                </p>
-                <h3 class="mb-30px">Lorem Ipsum</h3>
-                <img
-                    class="blog-image blog-image-square mb-30px mb-lg-50px"
-                    :src="greatMilenko"
-                    alt=""
-                />
-                <p class="mb-30px">
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                    Sequi cumque incidunt earum architecto dolor sit accusamus,
-                    magnam optio eius repellat labore molestias recusandae vitae
-                    delectus itaque. Facilis, et quis labore sint eligendi fuga
-                    unde quam quaerat dolorem voluptatum, alias rerum itaque.
-                    Nihil accusantium placeat consectetur numquam pariatur?
-                    Maxime, laborum doloribus? Lorem ipsum dolor sit, amet
-                    consectetur adipisicing elit. Sequi cumque incidunt earum
-                    architecto dolor sit accusamus, magnam optio eius repellat
-                    labore molestias recusandae vitae delectus itaque. Facilis,
-                    et quis labore sint eligendi fuga unde quam quaerat dolorem
-                    voluptatum, alias rerum itaque. Nihil accusantium placeat
-                    consectetur numquam pariatur? Maxime, laborum doloribus?
-                </p>
+
             </div>
             <div class="blog-single__bottom">
-                <div class="col-12 col-lg-3">
+                <div class="col-12 col-lg-4">
                     <div
                         class="d-flex flex-row flex-wrap-wrap justify-content-center align-items-center bg-primary pt-12px pb-12px pl-6px pr-6px column-gap-5px border-radius-12px text-white"
                     >
-                        <a class="hover-text" href="/kategoria/Bez-kategorii"
-                        >Bez kategorii,
-                        </a>
-                        <a class="hover-text" href="/kategoria/Bez-kategorii"
-                        >Lorem Ipsum,
-                        </a>
-                        <a class="hover-text" href="/kategoria/Bez-kategorii"
-                        >Lorem Ipsum Dolor</a
-                        >
+                        <Link>{{ post.blog_post_type }}</Link>
                     </div>
                 </div>
                 <div
-                    class="col-12 col-lg-3 d-flex justify-content-center align-items-center"
+                    class="col-12 col-lg-4 d-flex justify-content-center align-items-center"
                 >
                     <i class="fa fa-calendar mr-5px"></i>
-                    <span>2025-01-01</span>
+                    <span>{{post.updated_at}}</span>
                 </div>
                 <div
-                    class="col-12 col-lg-3 d-flex justify-content-center align-items-center"
+                    class="col-12 col-lg-4 d-flex justify-content-center align-items-center"
                 >
                     <i class="fa fa-user mr-5px"></i>
                     The Ringmaster
@@ -114,8 +74,8 @@ import blogBg from "~images/blog-bg.jpg";
                 <div
                     class="col-12 col-lg-3 d-flex justify-content-center align-items-center"
                 >
-                    <a href="#" class="hover-primary"
-                    ><i class="fa fa-arrow-left"></i> Widok Listy</a
+                    <Link :href="route('blog')" class="hover-primary"
+                    ><i class="fa fa-arrow-left"></i> Widok Listy</Link
                     >
                 </div>
             </div>
