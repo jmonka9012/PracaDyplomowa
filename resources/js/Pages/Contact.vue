e515ce79afa3cc33e2c80cc84b6511857994b266
Jacek Mońka <jmonka@edu.cdv.pl>
2025-05-01 15:25:36 +0200

    Stworzenie ramy do requesta formularza kontaktowego na /kontakt oraz podmiana placeholderowej treści

--- resources/js/Pages/Contact.vue
+++ resources/js/Pages/Contact.vue
@@ -5,6 +5,19 @@ import contactsMap from "~images/contacts_map.jpg";
 import facebookIcon from "~icons/facebook-circle-black.svg";
 import twitterIcon from "~icons/twitter-black.svg";
 import instagramIcon from "~icons/instagram-black.svg";
+
+import {reactive} from "vue";
+
+const contactForm = reactive({
+    name: null,
+    topic: null,
+    email: null,
+    message: null,
+});
+
+function SendContactEmail() {
+    console.log(contactForm);
+}
 </script>
 
 <template>
@@ -17,43 +30,41 @@ import instagramIcon from "~icons/instagram-black.svg";
             </div>
             <div class="col-12 col-lg-6 d-flex flex-column">
                 <a href="" class="sub-title sub-title-lprpl mb-17px"
-                    >Contact us</a
+                    >Kontakt</a
                 >
-                <h3 class="lh-1_4 mb-20px">In Touch with You</h3>
+                <h3 class="lh-1_4 mb-20px">Masz pytania?</h3>
                 <p class="mb-16px">
-                    Faucibus interdum posuere habitant morbi tristique senectus
-                    netus et malesuada. Euismod nisi porta lorem mollis aliquam
-                    ut porttitor.
+                    Masz pytania? Regulamin jest niejasny? Chcesz dopytać o to w jaki sposób organizujemy wydarzenia? Napisz do nas!
                 </p>
                 <div
                     class="d-flex flex-lg-row justify-content-between pb-16px mb-20px bb-1 b-secondary"
                 >
-                    <p class="fs-24 ff-krona">London</p>
-                    <p class="text-gray">3 Dufferin Street. London EC1Y 8NA</p>
+                    <p class="fs-24 ff-krona">Poznań</p>
+                    <p class="text-gray">ul. adres.pl 66-666</p>
                 </div>
                 <div
                     class="d-flex flex-lg-row justify-content-between mb-30px mb-lg-65px"
                 >
                     <div class="col-12 col-lg-6 d-flex flex-column">
-                        <a class="hover-primary ff-krona fs-14" href=""
-                            >mborkowski2@edu.cdv.pl</a
+                        <a class="hover-primary ff-krona fs-14" href="mailto:kontakt@eventmachen.pl"
+                            >kontakt@eventmachen.pl</a
                         >
-                        <a class="hover-primary ff-krona fs-14" href=""
-                            >mój numer xd</a
+                        <a class="hover-primary ff-krona fs-14" href="tel:+48111222333"
+                            >111 222 333</a
                         >
                     </div>
                     <div class="col-12 col-lg-6 d-flex justify-content-lg-end">
-                        <a class="morph-btn morph-btn-kont kont-icon" href="">
+                        <a class="morph-btn morph-btn-kont kont-icon" target="_blank" href="">
                             <div class="morph-btn__holder">
                                 <img :src="facebookIcon" />
                             </div>
                         </a>
-                        <a class="morph-btn morph-btn-kont kont-icon" href="">
+                        <a class="morph-btn morph-btn-kont kont-icon" target="_blank" href="">
                             <div class="morph-btn__holder">
                                 <img :src="twitterIcon" />
                             </div>
                         </a>
-                        <a class="morph-btn morph-btn-kont kont-icon" href="">
+                        <a class="morph-btn morph-btn-kont kont-icon" target="_blank" href="">
                             <div class="morph-btn__holder">
                                 <img :src="instagramIcon" />
                             </div>
@@ -61,22 +72,23 @@ import instagramIcon from "~icons/instagram-black.svg";
                     </div>
                 </div>
                 <div class="col-12 d-flex flex-column">
-                    <h4 class="fs-36 mb-20px">Drop us a line</h4>
-                    <form class="form" action="">
+                    <h4 class="fs-36 mb-20px">Wyślij e-mail</h4>
+                    <form class="form" @submit.prevent="SendContactEmail">
                         <div class="input-wrap col-12 col-lg-6">
-                            <input type="text" placeholder="Full Name*" />
+                            <input v-model="contactForm.name" type="text" required name="contact-name" placeholder="Twoje imie*" />
                         </div>
                         <div class="input-wrap col-12 col-lg-6">
-                            <input type="text" placeholder="Your Email*" />
+                            <input v-model="contactForm.email" type="text" required name="contact-mail" placeholder="Twój email*" />
                         </div>
                         <div class="input-wrap col-12">
-                            <input type="text" placeholder="Website" />
+                            <input v-model="contactForm.topic" type="text" required name="contact-topic" placeholder="Temat wiadomości*" />
                         </div>
                         <div class="input-wrap col-12">
                             <textarea
-                                name=""
+                                v-model="contactForm.message"
+                                name="contact-message"
                                 id=""
-                                placeholder="Your message"
+                                placeholder="Wiadomość"
                             ></textarea>
                         </div>
                         <input type="submit" value="wyślij" />
