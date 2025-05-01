fab0f47086ab8faef11ad451fd27c1ba7e9d7f10
Jacek Mońka <jmonka@edu.cdv.pl>
2025-05-01 15:37:09 +0200

    Dodanie więcej FE do AboutUs

--- resources/js/Pages/About-us.vue
+++ resources/js/Pages/About-us.vue
@@ -2,6 +2,7 @@
 import Tab from "@/Components/Partials/Tab.vue";
 import Tabs from "@/Components/Partials/Tabs.vue";
 import Hero from "@/Components/Sections/AboutUsHero.vue";
+import blogBg from "~images/blog-bg.jpg";
 
 import useAuth from '@/Utilities/useAuth'
 
@@ -19,6 +20,28 @@ const props = defineProps({
 <template>
 
     <Hero></Hero>
+    <section class="section-full mb-100px">
+        <div class="container container-full">
+            <div class="col-12 col-lg-6 pl-10px pr-10px pl-lg-0 pr-lg-0">
+                <img class="h-100 of-cover" :src="blogBg" alt="" />
+            </div>
+            <div
+                class="col-12 col-lg-6 pt-30px pb-30px pt-lg-100px pb-lg-100px pl-10px pr-10px pl-md-30px pr-md-30px pl-lg-100px pr-lg-100px"
+            >
+                <h1 class="mb-15px">Lorem ispum</h1>
+                <p class="mb-30px">
+                    <b>Lorem ipsum dolo</b>r sit, amet consectetur adipisicing
+                    elit. Dolorum, itaque. Recusandae cum quaerat explicabo.
+                    Dignissimos quibusdam itaque excepturi quod corporis.
+                </p>
+                <p>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    Dolor eveniet ab eum repellat distinctio veritatis
+                    voluptatum enim consectetur explicabo tempore!
+                </p>
+            </div>
+        </div>
+    </section>
     <section id="halls" class="overflow-hidden">
         <div class="container container-mid">
             <div class="col-lg-12">
@@ -96,6 +119,26 @@ const props = defineProps({
                     </Tab>
                 </Tabs>
             </div>
+        </div>
+    </section>
+    <section class="">
+        <div class="container justify-content-center">
+            <h1 class="mb-30px">Lorem Ipsum dolor sit amet</h1>
+            <p class="mb-50px">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil
+                officiis repudiandae totam? Sunt, ratione nam! Sed maxime et
+                quos odit repellendus consectetur ex nulla! Consequuntur magni a
+                possimus omnis laborum, deleniti incidunt, itaque voluptatem
+                odio est quia. Asperiores, facere exercitationem. Velit hic
+                placeat error quibusdam beatae consequatur doloribus enim
+                nesciunt. Lorem ipsum dolor sit amet, consectetur adipisicing
+                elit. Nihil officiis repudiandae totam? Sunt, ratione nam! Sed
+                maxime et quos odit repellendus consectetur ex nulla!
+                Consequuntur magni a possimus omnis laborum, deleniti incidunt,
+                itaque voluptatem odio est quia. Asperiores, facere
+                exercitationem. Velit hic placeat error quibusdam beatae
+                consequatur doloribus enim nesciunt.
+            </p>
         </div>
     </section>
 
