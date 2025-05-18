98e0a86f01f1e41afef1ea9f627022b14030bc8d
yen <notyourb42@gmail.com>
2025-05-18 21:11:04 +0200

    throttle z inertia, @jacek zobacz czy uda ci się naprawić buga

--- app/Http/Middleware/ThrottleWithError.php
+++ app/Http/Middleware/ThrottleWithError.php
@@ -9,6 +9,26 @@ use Inertia\Inertia;
 
 class ThrottleWithError extends ThrottleRequests
 {
+    // public function handle($request, Closure $next, $maxAttempts = 60, $decayMinutes = 1, $prefix = '')
+    // {
+    //     try {
+    //         return parent::handle($request, $next, $maxAttempts, $decayMinutes, $prefix);
+    //     } catch (ThrottleRequestsException $e) {
+    //         if ($request->header('X-Inertia')) {
+    //             $retryAfter = $e->getHeaders()['Retry-After'] ?? null;
+    //             $message = 'Zbyt wiele prób, spróbuj ponownie za ' . $retryAfter . ' sekund';
+
+    //             return response()->json([
+    //                 'message' => $message,
+    //                 'retryAfter' => $retryAfter,
+    //             ], 429)
+    //             ->withHeaders($e->getHeaders())
+    //             ->header('X-Inertia-Location', url()->current());
+    //         }
+
+    //         throw $e;
+    //     }
+    // }
     public function handle($request, Closure $next, $maxAttempts = 60, $decayMinutes = 1, $prefix = '')
     {
         try {
@@ -19,11 +39,17 @@ class ThrottleWithError extends ThrottleRequests
                 $message = 'Zbyt wiele prób, spróbuj ponownie za ' . $retryAfter . ' sekund';
 
                 return response()->json([
-                    'message' => $message,
-                    'retryAfter' => $retryAfter,
+                    'component' => $request->route()->getName(),
+                    'props' => [
+                        'errors' => [
+                            'throttle' => $message,
+                        ],
+                        'retryAfter' => $retryAfter,
+                    ],
+                    'url' => $request->path(),
                 ], 429)
                 ->withHeaders($e->getHeaders())
-                ->header('X-Inertia-Location', url()->current());
+                ->header('X-Inertia', true);
             }
 
             throw $e;
