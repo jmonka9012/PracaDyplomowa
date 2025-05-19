a049752fc07b5b03d9c46b533f39126b8506165a
yen <notyourb42@gmail.com>
2025-05-19 19:40:54 +0200

    throttle po raz N-ty, może tym razem zadziała

--- app/Http/Middleware/ThrottleWithError.php
+++ app/Http/Middleware/ThrottleWithError.php
@@ -9,22 +9,27 @@ use Inertia\Inertia;
 
 class ThrottleWithError extends ThrottleRequests
 {
-    public function handle($request, Closure $next, $maxAttempts = 60, $decayMinutes = 1, $prefix = '')
-    {
-        try {
-            return parent::handle($request, $next, $maxAttempts, $decayMinutes, $prefix);
-        } catch (ThrottleRequestsException $e) {
-            if ($request->header('X-Inertia')) {
-                $retryAfter = $e->getHeaders()['Retry-After'] ?? 60;
-                
-                return back()->withErrors([
-                    'throttle' => 'Zbyt wiele prób, spróbuj ponownie za'.$retryAfter.' sekund.',
-                ])
-                ->withHeaders($e->getHeaders())
-                ->setStatusCode(429);
-            }
-
-            throw $e;
+public function handle($request, Closure $next, $maxAttempts = 60, $decayMinutes = 1, $prefix = '')
+{
+    try {
+        return parent::handle($request, $next, $maxAttempts, $decayMinutes, $prefix);
+    } catch (ThrottleRequestsException $e) {
+        $retryAfter = $e->getHeaders()['Retry-After'] ?? 60;
+        
+        if ($request->expectsJson()) {
+            return response()->json([
+                'throttle' => 'Zbyt wiele prób, spróbuj ponownie za '.$retryAfter.' sekund.',
+                'retry_after' => $retryAfter
+            ], 429, $e->getHeaders());
         }
+
+        
+       return response(
+        'Zbyt wiele prób, spróbuj ponownie za '.$retryAfter.' sekund.',
+        429
+        )->withHeaders($e->getHeaders());
+
+        
     }
+}
 }
