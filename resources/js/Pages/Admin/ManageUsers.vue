60a13cdfa50da4124af0d5e5185e9b6992e6ff85
Jacek Mońka <jmonka@edu.cdv.pl>
2025-05-22 19:55:31 +0200

    Wstępne czytanie danych organizatorskich oraz PUT ze zmianą statusu

--- resources/js/Pages/Admin/ManageUsers.vue
+++ resources/js/Pages/Admin/ManageUsers.vue
@@ -3,8 +3,10 @@ import blogBg from "~images/blog-bg.jpg";
 import { router } from "@inertiajs/vue3";
 import { reactive } from "vue";
 import { Link } from "@inertiajs/vue3";
-
+import useAuth from "@/Utilities/useAuth";
 import HeroSmall from "@/Components/Sections/Hero-small.vue";
+import Collapse from "../../Components/Partials/Collapse.vue";
+
 
 const props = defineProps({
     users: {
@@ -41,6 +43,31 @@ function DeleteUser(id) {
         data: { user_id: id },
         preserveScroll: true,
         only: ['users']
+    })
+}
+
+function TranslateStatus(status) {
+    if (status === 'verified') {
+        return "Zweryfikowany"
+    } else if (status === 'denied') {
+        return "Odrzucony"
+    } else if (status === 'pending') {
+        return 'Oczekujący na zatwierdzenie'
+    }
+}
+
+function SetOrganizerStatus(value, userID) {
+    router.put(route('admin.users.organizers.change_status', {
+        id: userID // Tutaj przekazujemy ID do parametru URL
+    }), {
+        status: value // Tylko status w ciele żądania
+    }, {
+        onError: (err) => {
+            console.log('Błąd:', err);
+        },
+        onSuccess: (page) => {
+            console.log('Sukces:', page);
+        },
     })
 }
 
@@ -202,6 +229,48 @@ function DeleteUser(id) {
                     <div class="user-row__value">
                         <Link preserve-scroll method="delete" :only="['users']" @click="DeleteUser(user.id)" >Usuń</Link>
                     </div>
+                    <div v-if="user.organizer" class="user-row__value user-row__value--span" >
+                        <Collapse class="w-100">
+                        <template #trigger="{ isOpen }">
+                            <button class="btn btn-md">
+                                {{
+                                    isOpen
+                                        ? "Ukryj"
+                                        : "Pokaż szczegóły organizatora"
+                                }}
+                            </button>
+                        </template>
+                        <div class="content pt-20px">
+                            <div class="user-row user-row--head">
+                                <div>ID:</div>
+                                <div>Nazwa firmy:</div>
+                                <div>Adres:</div>
+                                <div>NIP:</div>
+                                <div>Bank:</div>
+                                <div>E-mail:</div>
+                                <div>Nr. telefonu:</div>
+                                <div>Status:</div>
+                            </div>
+                            <div class="user-row">
+                                <div class="user-row__value">{{user.organizer.id}}</div>
+                                <div class="user-row__value">{{user.organizer.company_name}}</div>
+                                <div class="user-row__value">{{user.organizer.address}}</div>
+                                <div class="user-row__value">{{user.organizer.tax_number}}</div>
+                                <div class="user-row__value">{{user.organizer.bank_account_number}}</div>
+                                <div class="user-row__value">{{user.organizer.email}}</div>
+                                <div class="user-row__value">{{user.organizer.phone_number}}</div>
+                                <div  class="user-row__value relative">
+                                    <div v-html="TranslateStatus(user.organizer.account_status)"></div>
+                                    <select @change="SetOrganizerStatus($event.target.value, user.organizer.id)" class="user-row__change-status" name="changeStatus" id="changeStatus">
+                                        <option :selected="user.organizer.account_status === 'verified'" :disabled="user.organizer.account_status === 'verified'" value="verified">Zweryfikowany</option>
+                                        <option :selected="user.organizer.account_status === 'denied'" :disabled="user.organizer.account_status === 'denied'" value="verified">Odrzucony</option>
+                                        <option :selected="user.organizer.account_status === 'pending'" :disabled="user.organizer.account_status === 'pending'" value="verified">Oczekujący</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                    </Collapse>
+                    </div>
                 </div>
             </div>
             <div class="event-pagination">
@@ -225,9 +294,26 @@ function DeleteUser(id) {
     width: 100%;
     display: grid;
     grid-template-columns: repeat(8, 1fr);
+    border: 1px solid black;
 
     &__value {
 
+        &--span {
+            grid-column: 1/-1;
+        }
+    }
+
+    &__change-status {
+        position: absolute;
+        transition: all .2s ease-in-out;
+        top: 0;
+        left: 0;
+        z-index: 5;
+
+        &:hover {
+            opacity: 1;
+            visibility: visible;
+        }
     }
 
     &--head {
