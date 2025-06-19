a9b1e5e8e50688435ec698888b4d3c1965f1c954
yen <notyourb42@gmail.com>
2025-06-19 15:01:25 +0200

    seeder z wszystkimi randomowymi seederami

---
+++ database/seeders/RandomSeeders.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class RandomSeeders extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $this->call([
+            BlogPostSeeder::class,
+            EventSeeder::class,
+            OrganizerSeeder::class,
+            SupportTicketSeeder::class,
+            UserSeeder::class
+        ]);
+    }
+}
