5d52ae7a3db497c3b688decf1af41712f7e12e6f
Materdeo <mateusz.borkowski22@gmail.com>
2025-06-30 22:26:06 +0200

    Add reactive to pending-events.vue

--- resources/js/Pages/Admin/PendingEvents.vue
+++ resources/js/Pages/Admin/PendingEvents.vue
@@ -5,13 +5,15 @@ import Tab from "@/Components/Partials/Tab.vue";
 import Tabs from "@/Components/Partials/Tabs.vue";
 import DatePicker from "@/Components/Partials/DatePicker.vue";
 import MultiSelect from "@/Components/Partials/MultiSelect.vue";
-import {Link} from "@inertiajs/vue3";
+import { Link } from "@inertiajs/vue3";
 import EventTable from "../../Components/Partials/EventTable/EventTable.vue";
 import { router } from "@inertiajs/vue3";
 import axios from "axios";
-import {onMounted} from "vue";
+import { onMounted } from "vue";
 
-const filterRequest = ({
+import { reactive, watch, computed, ref } from "vue";
+
+const filterRequest = reactive({
     event_name: null,
     event_genres: null,
     event_date: null,
@@ -37,7 +39,7 @@ const props = defineProps({
     genres: {
         required: true,
         type: Array,
-    }
+    },
 });
 
 let genres = [];
@@ -53,13 +55,13 @@ console.log(genres);
 
 function submitFilterRequest() {
     const params = new URLSearchParams(window.location.search);
-    if (params.get('tabName')) {
-        filterRequest.tabName = params.get('tabName');
+    if (params.get("tabName")) {
+        filterRequest.tabName = params.get("tabName");
     }
 
-    router.get(route("admin.events"),filterRequest, {
+    router.get(route("admin.events"), filterRequest, {
         replace: true,
-        only: ['events', 'pending_events'],
+        only: ["events", "pending_events"],
         preserveScroll: true,
         preserveState: true,
     });
@@ -75,13 +77,15 @@ function submitFilterRequest() {
                     <h3 class="mb-30px">Zatwierdzone wydarzenia</h3>
                     <form
                         class="select-filters col-12 col-lg-8 align-items-center d-flex flex-column"
-                        @submit.prevent="submitFilterRequest()">
+                        @submit.prevent="submitFilterRequest()"
+                    >
                         <div class="input-wrap relative col-12">
                             <input
                                 v-model="filterRequest.event_name"
                                 class="search-input pl-10px"
                                 placeholder="Szukaj po nazwie"
-                                type="text"/>
+                                type="text"
+                            />
                             <i class="fa fa-search search-icon"></i>
                         </div>
                         <MultiSelect
@@ -100,37 +104,53 @@ function submitFilterRequest() {
                             value="Filtruj"
                         />
                     </form>
-                    <EventTable v-show="Object.keys(props.events.data).length>0" :events="props.events.data" class="mb-40px col-12"></EventTable>
-                    <div v-show="Object.keys(props.events.data)===0">Brak wyników</div>
+                    <EventTable
+                        v-show="Object.keys(props.events.data).length > 0"
+                        :events="props.events.data"
+                        class="mb-40px col-12"
+                    ></EventTable>
+                    <div v-show="Object.keys(props.events.data) === 0">
+                        Brak wyników
+                    </div>
                     <div class="event-pagination">
                         <ul class="ml-auto mr-auto">
-                            <li v-for="page in props.events.meta.links"
+                            <li
+                                v-for="page in props.events.meta.links"
                                 :key="page"
                                 :class="{ 'page-current': page.active }"
-                                class="page">
-                                <Link preserve-state :only="['events']" :href="page.url" v-html="page.label"></Link>
+                                class="page"
+                            >
+                                <Link
+                                    preserve-state
+                                    :only="['events']"
+                                    :href="page.url"
+                                    v-html="page.label"
+                                ></Link>
                             </li>
                         </ul>
                     </div>
                 </Tab>
                 <Tab title="Oczekujące wydarzenia">
-                    <h3 class="mb-30px">Wydarzenia czekające na zatwierdzenie</h3>
+                    <h3 class="mb-30px">
+                        Wydarzenia czekające na zatwierdzenie
+                    </h3>
                     <form
                         class="select-filters col-12 col-lg-8 align-items-center d-flex flex-column"
-                        @submit.prevent="submitFilterRequest()">
+                        @submit.prevent="submitFilterRequest()"
+                    >
                         <div class="input-wrap relative col-12">
                             <input
                                 v-model="filterRequest.pending_name"
                                 class="search-input pl-10px"
                                 placeholder="Szukaj po nazwie"
-                                type="text"/>
+                                type="text"
+                            />
                             <i class="fa fa-search search-icon"></i>
                         </div>
                         <MultiSelect
                             v-model="filterRequest.pending_genres"
                             :options="genres"
                             placeholder="Wybierz kategorie"
-
                         ></MultiSelect>
                         <DatePicker
                             v-model="filterRequest.pending_date"
@@ -143,15 +163,31 @@ function submitFilterRequest() {
                             value="Filtruj"
                         />
                     </form>
-                    <EventTable v-show="Object.keys(props.pending_events.data).length>0" :events="props.pending_events.data" :pending="true" class="mb-40px col-12"></EventTable>
-                    <div v-show="Object.keys(props.pending_events.data)===0">Brak wyników</div>
+                    <EventTable
+                        v-show="
+                            Object.keys(props.pending_events.data).length > 0
+                        "
+                        :events="props.pending_events.data"
+                        :pending="true"
+                        class="mb-40px col-12"
+                    ></EventTable>
+                    <div v-show="Object.keys(props.pending_events.data) === 0">
+                        Brak wyników
+                    </div>
                     <div class="event-pagination">
                         <ul class="ml-auto mr-auto">
-                            <li v-for="page in props.pending_events.meta.links"
+                            <li
+                                v-for="page in props.pending_events.meta.links"
                                 :key="page"
                                 :class="{ 'page-current': page.active }"
-                                class="page">
-                                <Link preserve-state :only="['pending_events']" :href="page.url" v-html="page.label"></Link>
+                                class="page"
+                            >
+                                <Link
+                                    preserve-state
+                                    :only="['pending_events']"
+                                    :href="page.url"
+                                    v-html="page.label"
+                                ></Link>
                             </li>
                         </ul>
                     </div>
