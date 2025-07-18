a734fcca25de47cdcd19dcc6c6843d40ecfee441
Jacek Mońka <jacek@bizwebstudio.pl>
2025-07-18 20:28:28 +0200

    Usunięcie niepotrzebnego kodu z Events.vue

--- resources/js/Components/Sections/Events.vue
+++ resources/js/Components/Sections/Events.vue
@@ -1,6 +1,4 @@
 <script setup>
-import { reactive, ref } from "vue";
-
 import Event from "../Partials/Event.vue";
 
 const props = defineProps({
@@ -10,20 +8,6 @@ const props = defineProps({
     },
 });
 
-const scrollContainer = ref(null);
-const scrollAmount = 50;
-
-const scrollLeft = () => {
-    if (scrollContainer.value) {
-        scrollContainer.value.scrollBy(-scrollAmount, 0);
-    }
-};
-
-const scrollRight = () => {
-    if (scrollContainer.value) {
-        scrollContainer.value.scrollBy(scrollAmount, 0);
-    }
-};
 </script>
 <template>
     <Event :event="event" v-for="event in props.events" :key="event.id"></Event>
